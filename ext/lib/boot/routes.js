9bbab5cf1befd03a2df3535438b6beb22bd2dff5
Matías Lescano <mjlescano@protonmail.com>
2017-05-22 13:07:38 -0300

    fix traducir urls #128

--- ext/lib/boot/routes.js
+++ ext/lib/boot/routes.js
@@ -4,4 +4,12 @@ module.exports = function () {
   urlBuilder.register('site.topic', '/:forum/consulta/:id')
   urlBuilder.register('site.help', '/ayuda')
   urlBuilder.register('site.help.article', '/ayuda/:article')
+  urlBuilder.register('site.notifications', '/notificaciones')
+
+  urlBuilder.register('settings', '/ajustes')
+  urlBuilder.register('settings.profile', '/ajustes/perfil')
+  urlBuilder.register('settings.password', '/ajustes/contrasena')
+  urlBuilder.register('settings.notifications', '/ajustes/notificaciones')
+  urlBuilder.register('settings.forums', '/ajustes/administrar')
+  urlBuilder.register('settings.user-badges', '/ajustes/user-badges')
 }
