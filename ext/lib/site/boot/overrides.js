73c53a683a1b12ba8a5ea940aa5e7b654d05e11c
Fran <francisco@democracyos.io>
2017-07-01 19:20:31 -0300

    cerrar comentarios al cerrar consulta

--- ext/lib/site/boot/overrides.js
+++ ext/lib/site/boot/overrides.js
@@ -12,7 +12,11 @@ import TopicLayoutExt from 'ext/lib/site/topic-layout/component'
 import * as Help from 'lib/site/help/component'
 import HelpExt from 'ext/lib/site/help/component'
 
+import * as Comments from 'lib/site/topic-layout/topic-article/comments/component'
+import CommentsExt from 'ext/lib/site/topic-layout/topic-article/comments/component'
+
 HomeForum.default = HomeForumExt
 HomeMultiForum.default = HomeMultiForumExt
 TopicLayout.default = TopicLayoutExt
 Help.default = HelpExt
+Comments.default = CommentsExt
