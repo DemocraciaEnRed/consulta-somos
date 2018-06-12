becbb9950570479c70a125020e8dfd53d65f071b
florlafuente <lafuente.florencia@gmail.com>
2018-06-12 15:10:22 -0300

    Cambia topic-action a componente funcional

--- ext/lib/site/topic-layout/topic-article/topic-action/component.js
+++ ext/lib/site/topic-layout/topic-article/topic-action/component.js
@@ -1,41 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Vote from 'lib/site/topic-layout/topic-article/vote/component'
 import Poll from 'lib/site/topic-layout/topic-article/poll/component'
 import Cause from 'lib/site/topic-layout/topic-article/cause/component'
 import Slider from 'lib/site/topic-layout/topic-article/slider/component'
 import Hierarchy from 'lib/site/topic-layout/topic-article/hierarchy/component'
 
-export default class extends Component {
-  render () {
-    const { topic } = this.props
-    return (
-      <div className='topic-article-content'>
-      {
-        topic.action.method && topic.action.method === 'vote' && (
-          <Vote topic={topic} />
-        )
-      }
-      {
-        topic.action.method && topic.action.method === 'poll' && (
-          <Poll topic={topic} />
-        )
-      }
-      {
-        topic.action.method && topic.action.method === 'cause' && (
-          <Cause topic={topic} />
-        )
-      }
-      {
-        topic.action.method && topic.action.method === 'slider' && (
-          <Slider topic={topic} />
-        )
-      }
-      {
-        topic.action.method && topic.action.method === 'hierarchy' && (
-          <Hierarchy topic={topic} />
-        )
-      }
-      </div>
-    )
-  }
-}
+export default ({ topic }) => (
+  <div className='topic-article-content'>
+    {(() => {
+      switch(topic.action.method) {
+        case 'vote':
+          return  <Vote topic={topic} />
+        case 'poll':
+          return  <Poll topic={topic} />
+        case 'cause':
+          return <Cause topic={topic} />
+        case 'slider':
+          return <Slider topic={topic} />
+        case 'hierarchy':
+          return <Hierarchy topic={topic} />
+        }
+    })()}
+  </div>
+)
