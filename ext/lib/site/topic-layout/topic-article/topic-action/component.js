74dd8232b072459089c0e64666e37a81f44301d1
florlafuente <lafuente.florencia@gmail.com>
2018-06-13 16:25:59 -0300

    Hace hoja de estilos separadas para cada componente

--- ext/lib/site/topic-layout/topic-article/topic-action/component.js
+++ ext/lib/site/topic-layout/topic-article/topic-action/component.js
@@ -8,6 +8,7 @@ import Hierarchy from 'lib/site/topic-layout/topic-article/hierarchy/component'
 export default ({ topic }) => (
   <div className='topic-article-content topic-article-action'>
     <h3 className='topic-action-title'>{'¿Qué propuestas tienen para mejorar los contenidos e información en el sitio web sobre la cuestión de Archivos y Derechos Humanos?'}</h3>
+    {console.log(topic)}
     {!!topic.voted &&
       <div className='topic-action-voted'>
         {topic.open &&
