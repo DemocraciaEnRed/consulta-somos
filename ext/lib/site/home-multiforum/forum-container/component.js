10df36934c76602458eb2bcd21ff12534c920f79
florlafuente <lafuente.florencia@gmail.com>
2018-06-22 11:27:55 -0300

    Corrige estilos en padding de container

--- ext/lib/site/home-multiforum/forum-container/component.js
+++ ext/lib/site/home-multiforum/forum-container/component.js
@@ -3,7 +3,7 @@ import CardsSlider from 'ext/lib/site/cards-slider/component'
 import ForumCard from '../forum-card/component'
 
 export default ({ forum }) => (
-  <div className='container'>
+  <div className='container forum-card-container'>
     <ForumCard forum={forum} />
     <div className='forum-slider-wrapper'>
       <h4 className='forum-slider-title'>Los ejes que comprenden esta consulta son:</h4>
