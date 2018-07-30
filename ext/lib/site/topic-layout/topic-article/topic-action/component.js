1233bcb2672f768cf894e0eaf779918262dd9b17
luarar <laurarh.971@gmail.com>
2018-07-30 14:57:23 -0300

    cambia parrafo de alert y agrega estilos nuevos

--- ext/lib/site/topic-layout/topic-article/topic-action/component.js
+++ ext/lib/site/topic-layout/topic-article/topic-action/component.js
@@ -13,7 +13,7 @@ export default ({ topic }) => (
     {!!topic.voted &&
       <div className='topic-action-voted'>
         {topic.open &&
-          <p>Ya votaste en la consulta</p>
+          <p><i className='icon-clock' />Ya votaste en este eje</p>
         }
         <div>
           <span>Elegiste la opción:</span>
