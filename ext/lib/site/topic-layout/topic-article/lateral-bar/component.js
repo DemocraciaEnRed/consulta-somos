218a5aa59ea70c2494b413696ed09247185603f4
luarar <laurarh.971@gmail.com>
2018-08-06 14:53:32 -0300

    agrega nuevo componente menu lateral

---
+++ ext/lib/site/topic-layout/topic-article/lateral-bar/component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default ({ forum }) => (
+  <div className='lateral-bar-container'>
+    <ul className='lateral-bar-list'>
+        <li className='lateral-bar-item'>Hola</li>
+        <li className='lateral-bar-item'>Chau</li>
+    </ul>
+  </div>
+)
