ad2a464ed37da358647c46984a7d53c85f9876c0
florlafuente <lafuente.florencia@gmail.com>
2018-08-08 11:02:23 -0300

    Erase log

--- ext/lib/site/topic-layout/topic-article/lateral-bar/component.js
+++ ext/lib/site/topic-layout/topic-article/lateral-bar/component.js
@@ -3,8 +3,6 @@ import Jump from 'jump.js'
 
 export default ({ nodes }) => (
   <div className={`lateral-bar-container ${nodes.length > 0 ? 'large' : 'small'}`}>
-
-  {console.log(nodes.length)}
     <ul className='lateral-bar-list'>
       {nodes.map((node,i) => (
         <li className='lateral-bar-item' key={i}>
