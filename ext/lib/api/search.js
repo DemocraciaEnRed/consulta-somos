e80f4a90308c2ef0a8939c7a913004ec6ea95c47
slorenzo <SebastianLorenzo@gmail.com>
2018-12-11 20:31:26 -0300

    Agregado search/authors endpoint

--- ext/lib/api/search.js
+++ ext/lib/api/search.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const debug = require('debug')
-const log = debug('democracyos:ext:api:filters')
+const log = debug('democracyos:ext:api:search')
 const models = require('lib/models')
 const api = require('../db-api')
 
@@ -10,8 +10,24 @@ const Forum = models.Forum
 const app = module.exports = express()
 
 app.get(
+  '/authors',
+  function(req, res, next) {
+    log('Find authors')
+    return Promise.all([Forum.find().distinct('extra.owner'), Topic.find().distinct('author')])
+      .then((response) => {
+        const authors = [...response[0], ...response[1]]
+          .filter( author => author !== '')
+        return res.json(authors)
+      })
+      .catch(next)
+  }
+)
+
+app.get(
   '/:string/:kind/:author?',
   function(req, res, next) {
+    log('Find by text on Forums and Topics')
+
     const {
       string,
       kind,
@@ -21,14 +37,14 @@ app.get(
     const isConsulta = kind.includes('consulta')
     const isEje = kind.includes('eje')
 
+    if (!(isConsulta || isEje)) next(new Error('Invalid kind argument'))
+
     const regex = createRegex(string)
     const regexAuthor = createRegex(author || '')
 
     const mainQuery = { '$and': [] }
     const simpleQuery = { '$or' : [] }
     const authorQuery = { '$or': [] }
-
-    if (!(isConsulta || isEje)) next(new Error('Invalid kind argument'))
 
     if (isConsulta) {
       simpleQuery['$or'].push({ name: regex })
@@ -47,15 +63,14 @@ app.get(
       }
     }
 
-
     mainQuery['$and'].push(simpleQuery)
     if (authorQuery['$or'].length) {
       mainQuery['$and'].push(authorQuery)
     }
 
-    return Promise.all([Forum.find(mainQuery), Topic.find(mainQuery)])
+    return Promise.all([Forum.find(mainQuery), Topic.find(mainQuery).populate('forum')])
       .then((response) => {
-        return res.json(response)
+        return res.json([...response[0], ...response[1]])
       })
       .catch(next)
   }
